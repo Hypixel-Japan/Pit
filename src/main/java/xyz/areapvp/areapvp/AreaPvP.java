7cb43bc8cbc0342c231de834a10050eafe5a2cb1
peyang-Celeron <42040068+peyang-Celeron@users.noreply.github.com>
2020-11-15 14:33:35 +0900

    いろいろ

--- src/main/java/xyz/areapvp/areapvp/AreaPvP.java
+++ src/main/java/xyz/areapvp/areapvp/AreaPvP.java
@@ -1,26 +1,34 @@
 package xyz.areapvp.areapvp;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AreaPvP extends JavaPlugin {
+import java.util.HashMap;
+
+public final class AreaPvP extends JavaPlugin
+{
     public static FileConfiguration config;
-    public static boolean Build = false;
     public static AreaPvP plugin;
-    @Override
-    public void onEnable() {
-        Bukkit.getPluginManager( ).registerEvents( new Block() , this );
-        Bukkit.getPluginManager( ).registerEvents( new Kill() , this);
-        Bukkit.getPluginManager( ).registerEvents( new PlayerJoin() , this);
-        Bukkit.getPluginManager().registerEvents(  new NoFall() , this);
-        getCommand( "areapvp" ).setExecutor( new Commands() );
-        saveDefaultConfig();
-        plugin = this;
-        config = getConfig();
-    }
+    public static HashMap<Location, Integer> blockPlace;
+    public static Timer timer;
+
     public static AreaPvP getPlugin()
     {
         return plugin;
+    }
+
+    @Override
+    public void onEnable()
+    {
+        Bukkit.getPluginManager().registerEvents(new Events(), this);
+        getCommand("areapvp").setExecutor(new Commands());
+        saveDefaultConfig();
+        plugin = this;
+        config = getConfig();
+        blockPlace = new HashMap<>();
+        timer = new Timer();
+        timer.runTaskTimerAsynchronously(this, 0L, 20L); //1秒に1回実行
     }
 }
